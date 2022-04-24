d15334c0c5fdcec206617e0db463ad477ec0df88
Arellano-Jann <jannrellano@nevada.unr.edu>
2022-04-23 20:48:08 -0700

    fixed cities and added main

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,22 @@
 // #include "../headers/GraphNode.h"
 
 #include <iostream>
+#include <fstream>
 
 int main(){
     int mpg = 40;
     Graph<std::string> graph;
-    graph.add("A", "B", mpg);
-    
+    // graph.add("A", "B", mpg);
+    // graph.printAdjecencyMatrix();
+
+    std::ifstream file;
+    file.open("cities.txt");
+    std::string city1;
+    std::string city2;
+    int distance;
+    while (file >> city1 >> city2 >> distance){
+        graph.add(city1, city2, distance);
+    }
+    graph.printAdjecencyMatrix();
     return 0;
 }
